21d5d85d4407106baf5c1bcc00e92f5dcf22267c
Watanabe Naoki <103019604+Wata-Naoki@users.noreply.github.com>
2022-10-05 11:08:26 +0900

    ABC102A_py

---
+++ ABC102A_py.py
@@ -0,0 +1,55 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyM1q5xx9HairqmLExCDb4Fw",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Wata-Naoki/atcoder/blob/main/ABC102A_py.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 1,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "GkH06Yjt4-ih",
+        "outputId": "e91c8aa0-983e-4ae0-e2f3-bbf3ec2fbc6a"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "6\n",
+            "6\n"
+          ]
+        }
+      ],
+      "source": [
+        "n = int(input())\n",
+        "print(2*n if n%2 else n)"
+      ]
+    }
+  ]
+}
