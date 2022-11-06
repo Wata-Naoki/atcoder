8377553e571b0cc89cd01e330767ed5b5110b493
Watanabe Naoki <103019604+Wata-Naoki@users.noreply.github.com>
2022-11-06 12:03:14 +0900

    アルゴリズムの勉強/ABC140a

---
+++ ABC140a.py
@@ -0,0 +1,56 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyO9XrGwwtfmxQrkv1sOv2J2",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Wata-Naoki/atcoder/blob/main/ABC140a.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 2,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "QWKtzQd64chv",
+        "outputId": "2ff79ca2-463c-4f4f-f210-9d7d61cb0799"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "1\n",
+            "1\n"
+          ]
+        }
+      ],
+      "source": [
+        "n=int(input())\n",
+        "\n",
+        "print(n**3)"
+      ]
+    }
+  ]
+}
