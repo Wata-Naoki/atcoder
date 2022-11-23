9e8c554c21a548e0d13a426a928225cdcf40d702
Watanabe Naoki <103019604+Wata-Naoki@users.noreply.github.com>
2022-11-23 11:41:32 +0900

    アルゴリズムの勉強/ABC157a

---
+++ ABC157a.py
@@ -0,0 +1,56 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNqO35ACLJylB0GzV/ym0Hk",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Wata-Naoki/atcoder/blob/main/ABC157a.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 4,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "kC3lK8ylS7az",
+        "outputId": "50008d25-8749-441c-dfa4-62d63af015fc"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "100\n",
+            "50\n"
+          ]
+        }
+      ],
+      "source": [
+        "import math\n",
+        "n=int(input())\n",
+        "print(math.ceil(n/2))\n"
+      ]
+    }
+  ]
+}
