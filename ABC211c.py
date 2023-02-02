4fa1ad485cb9b8fadcd099c00aa521f02d8162da
Wata-Naoki <a8120227@gsuite.si.aoyama.ac.jp>
2023-02-02 14:23:39 +0900

    ABC211c/アルゴリズムの勉強/2023/02/02

--- ABC211c.py
+++ ABC211c.py
@@ -5,17 +5,17 @@ mod = 10**9+7
 s = input()
 t = '*chokudai'
 dp = Counter()
-print(dp)
+# print(dp)
 dp['*'] = 1
 for char in s:
     if char in t:
         #   ここで、charの前の文字を取得する。
         char_prev = t[t.index(char)-1]
-        print('1', char, char_prev)
-        #   ここで、charの前の文字のdpを取得する。
+        # print('1', char, char_prev)
+        #   ここで、charの前の文字のdpを取得する。文字列を足していく。そして、カウントする。
         dp[char] += dp[char_prev]
-        print('2', dp)
+        # print('2', dp)
         #   ここで、charのdpを更新する。
         dp[char] %= mod
-        print('3', dp)
-print(dp)
+        # print('3', dp)
+print(dp['i'])
