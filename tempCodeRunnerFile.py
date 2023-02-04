8314709e64ca2d4df77869cd9b1a115c40bf99c9
Wata-Naoki <a8120227@gsuite.si.aoyama.ac.jp>
2023-02-04 11:57:00 +0900

    ABC213a/アルゴリズムの勉強/2023/02/04

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,15 +1,3 @@
-
-
-n, m = map(int, input().split())
-a = [int(n) for n in input().split()]
-b = [int(n) for n in input().split()]
-
-
-diff = []
-
-for i in range(n):
-    for j in range(m):
-        diff.append(abs(a[i]-b[j]))
-
-# print(diff)
-print(min(diff))
+a=3
+b=6
+print(b^a)
