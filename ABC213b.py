c2555802a3b311c4114594e88bf9f1290fb315bd
Wata-Naoki <a8120227@gsuite.si.aoyama.ac.jp>
2023-02-04 12:08:40 +0900

    ABC213b/アルゴリズムの勉強/2023/02/04

--- ABC213b.py
+++ ABC213b.py
@@ -4,6 +4,6 @@ a_list = [int(n) for n in input().split()]
 tmp_list = a_list.copy()
 # ソートしたいので、a_listをソートする
 a_list.sort()
-print(a_list)
-print(tmp_list)
-print(tmp_list.index(a_list[-2]))
+# print(a_list)
+# print(tmp_list)
+print(tmp_list.index(a_list[-2])+1)
