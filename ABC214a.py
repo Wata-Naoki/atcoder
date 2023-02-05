708dbf64aa1cbf168cc52d8c882d8fb97e1f4e90
Wata-Naoki <a8120227@gsuite.si.aoyama.ac.jp>
2023-02-05 10:53:56 +0900

    ABC214a/アルゴリズムの勉強/2023/02/05

--- ABC214a.py
+++ ABC214a.py
@@ -2,7 +2,7 @@
 n = int(input())
 if n >= 1 and n <= 125:
     print(4)
-elif n <= 126 and n <= 211:
+elif n >= 126 and n <= 211:
     print(6)
-elif n <= 212 and n <= 214:
+elif n >= 212 and n <= 214:
     print(8)
