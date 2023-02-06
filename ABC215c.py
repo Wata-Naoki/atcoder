624adbc8e79a2db74456c4408677261bf30117a8
Wata-Naoki <a8120227@gsuite.si.aoyama.ac.jp>
2023-02-06 12:06:18 +0900

    ABC215c/アルゴリズムの勉強/2023/02/06

---
+++ ABC215c.py
@@ -0,0 +1,29 @@
+# 入力の受け取り(Kも文字列として)
+import itertools
+S, K = map(str, input().split())
+# Kを整数に変換
+K = int(K)
+
+# 作れる文字を格納するセット(リストだと重複するものも入ってしまうためセットを使う)
+S_set = set()
+
+# itertoolsをインポート
+# seq=(0,1,2),(0, 2, 1),(1, 0, 2),(1, 2, 0),...
+for seq in itertools.permutations(range(len(S))):
+    # 作った文字列を記録する変数
+    S_tmp = ""
+    # seqを使って文字を作成
+    # 例：seq=(2,0,1)ならS_tmp=Sの2文字目+Sの0文字目+Sの1文字目
+    for i in seq:
+        S_tmp += S[i]
+    # できた文字をS_setに格納(setへの追加はappendではなくaddであることに注意)
+    S_set.add(S_tmp)
+
+# ソートするためにリストへ変換
+S_list = list(S_set)
+
+# 辞書順にソート
+S_list.sort()
+
+# K-1番目の要素を出力
+print(S_list[K-1])
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,10 +1,3 @@
-
-n=int(input())
-
-for i in range(n):
-    if n<=1:
-        print(0)
-        break
-    if 2**i>n:
-        print(i-1)
-        break
+import itertools
+for seq in itertools.permutations(range(3)):
+    print(seq)
