f3dca6e0a88b72b29ee5487ab9ed2690c1da66a8
Wata-Naoki <a8120227@gsuite.si.aoyama.ac.jp>
2023-02-09 17:42:18 +0900

    ABC218/アルゴリズムの勉強/2023/02/09

--- ABC218b.py
+++ ABC218b.py
@@ -2,7 +2,7 @@
 p = [int(x) for x in input().split()]
 # アルファベットのリストを作成
 alphabet = [chr(i) for i in range(97, 97+26)]
-print(alphabet)
+# print(alphabet)
 ans = ''
 for val in p:
     ans += alphabet[val-1]
