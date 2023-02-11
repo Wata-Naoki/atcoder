da36b91b1f033d84681b7c7c1f46a14afa842b78
Wata-Naoki <a8120227@gsuite.si.aoyama.ac.jp>
2023-02-11 23:37:48 +0900

    ABC220b/アルゴリズムの勉強/2023/02/11

--- ABC220b.py
+++ ABC220b.py
@@ -2,8 +2,11 @@
 
 # kの進数を10進数に変換する関数
 def k_to_10(k, n):
+    #   n[::-1]でnを逆順にする
     ans = 0
+    #  enumerateでインデックスと要素を取得
     for i, num in enumerate(n[::-1]):
+        #   int(num)で文字列を数値に変換
         ans += int(num) * (k**i)
     return ans
 
