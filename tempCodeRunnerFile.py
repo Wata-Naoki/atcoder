770b8e825ed1dcb8e29d00abc73912e8042eb8c4
Wata-Naoki <a8120227@gsuite.si.aoyama.ac.jp>
2023-02-12 18:35:56 +0900

    ABC221b/アルゴリズムの勉強/2023/02/12

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,10 @@
-a, b = map(int, input().split())
+s = input()
+t = input()
 
-if a == b:
-    print(1)
-else:
-    ans = 32**(a-b)
-    print(ans)
+count = 0
+for i in range(len(s)-1):
+    if s[i] != t[i]:
+        count += 1
+    if count >= 2:
+        break
+print('No' if count >= 2 else 'Yes')
