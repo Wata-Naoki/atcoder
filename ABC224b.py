e19ddf69b046096cfcfb4005e74a4dc6b24a072e
Wata-Naoki <a8120227@gsuite.si.aoyama.ac.jp>
2023-02-17 01:40:39 +0900

    comment

--- ABC224b.py
+++ ABC224b.py
@@ -7,7 +7,7 @@ for r2 in range(h):
         for c2 in range(w):
             for c1 in range(c2):
                 if a[r1][c1] + a[r2][c2] > a[r1][c2] + a[r2][c1]:
-
+                    #  この条件を満たす(r1, c1), (r2, c2)が存在するならば、Noを出力して終了
                     print('No')
                     exit()
 
