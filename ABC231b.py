cafa3eea94ff8719f6754d553ea7c651c0dfcbb4
Wata-Naoki <a8120227@gsuite.si.aoyama.ac.jp>
2023-02-26 22:18:11 +0900

    ABC231のa,b

--- ABC231b.py
+++ ABC231b.py
@@ -1,9 +1,9 @@
-n=int(input())
 import collections
+n=int(input())
 n_list=[]
 for _ in range(n):
     n_list.append(input())
 
 # リストの一番出現数が多いものを出力する
 c = collections.Counter(n_list)
-print(c.most_common()[0][0])
+print(c.most_common()[0][0]) 
