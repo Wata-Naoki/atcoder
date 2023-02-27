aac578909b7d5ec438fba948134aa7ce1306eb04
Wata-Naoki <a8120227@gsuite.si.aoyama.ac.jp>
2023-02-27 13:10:19 +0900

    ABC232b/アルゴリズムの勉強/2023/02/27

--- ABC232b.py
+++ ABC232b.py
@@ -3,9 +3,12 @@ s=list(input())
 t=list(input())
 st=set()
 for i in range(len(s)):
+    # ordのaは97で、zは122
+    # 26で足して26で割った余りをとることで、zからaに戻ることを考慮する
     k=(ord(t[i])-ord(s[i]) + 26) % 26
     st.add(k)
 
+# 全ての距離が等しければ、Yes
 if len(st) == 1:
     print('Yes')
 else:   
