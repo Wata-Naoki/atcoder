70bbd8b582c27d727320ade2d244b3aa4b29bff5
Wata-Naoki <a8120227@gsuite.si.aoyama.ac.jp>
2023-03-05 01:17:29 +0900

    ABC238a/アルゴリズムの勉強/2023/03/05

--- ABC236a.py
+++ ABC236a.py
@@ -1,6 +1,10 @@
 s=input()
 a, b=map(int, input().split())
-tmp_b=s[a]
-s[a]=s[b]
-s[b]=tmp_b
-print(s)
+s_list=list(s)
+tmp_a=s[a-1]
+tmp_b=s[b-1]
+# s[a]=tmp_b
+s_list[a-1]=tmp_b
+s_list[b-1]=tmp_a
+ans="".join(s_list)
+print(ans)
