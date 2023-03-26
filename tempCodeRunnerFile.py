253b2667191462d16fd45b7d49c8c7e6baf28559
Wata-Naoki <a8120227@gsuite.si.aoyama.ac.jp>
2023-03-27 00:17:49 +0900

    ABC250b/アルゴリズムの勉強/2023/03/27

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,13 @@
-a, b, k = map(int, input().split())
+h, w = map(int, input().split())
+r, c = map(int, input().split())
 
-tmp_a = a
-count = 0
-while tmp_a < b:
-    tmp_a *= k
-    count += 1
-print(count)
+ans = 0
+if 1 <= r - 1:
+    ans += 1
+if r + 1 <= h:
+    ans += 1
+if 1 <= c - 1:
+    ans += 1
+if c + 1 <= w:
+    ans += 1
+print(ans)
